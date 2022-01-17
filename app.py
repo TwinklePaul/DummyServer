52337171ae967cfceaf2397eb6ecb600643c195b
Twinkle <twinkle@MacBook-Pro.local>
2022-01-17 15:04:53 +0530

    Added Json formats for Home Page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from mock_suppliers import *
 from mock_chargers import *
 from mock_regions import *
@@ -143,48 +143,303 @@ def find_all_chargers(filter):
     }
     ]
 
+#####################################################################################
+################################## HOME PAGE ##################################
+#####################################################################################
 
-@app.route('/')
+
+@app.route('/', methods=['POST', 'GET'])
 def find_regionwise_suppliers(filter):
     '''
+        if post:
         filter of following format:
         {
             start_date: '',
             end_date: '',
         }
+
+        if get:
+            return all
     '''
-    return [{
-        'suppliers': [{
-            'supplier_id': supplier_active_01['supplier_id'],
-            'supplier_name': supplier_active_01['supplier_name'],
-        },
-            {
-            'supplier_id': supplier_active_02['supplier_id'],
-            'supplier_name': supplier_active_02['supplier_name'],
-        },
-            {
-            'supplier_id': supplier_active_03['supplier_id'],
-            'supplier_name': supplier_active_03['supplier_name'],
-        }],
+    if request.method == 'POST':
+        return [{
+            'suppliers': [{
+                'supplier_id': supplier_active_01['supplier_id'],
+                'supplier_name': supplier_active_01['supplier_name'],
+            },
+                {
+                'supplier_id': supplier_active_02['supplier_id'],
+                'supplier_name': supplier_active_02['supplier_name'],
+            },
+                {
+                'supplier_id': supplier_active_03['supplier_id'],
+                'supplier_name': supplier_active_03['supplier_name'],
+            }],
 
-        'regions': [{
-            'region_id': region_01['region_id'],
-            'region_name': region_01['region_name'],
-        },
-            {
-            'region_id': region_02['region_id'],
-            'region_name': region_02['region_name'],
-        }
-        ],
+            'regions': [{
+                'region_id': region_01['region_id'],
+                'region_name': region_01['region_name'],
+            },
+                {
+                'region_id': region_02['region_id'],
+                'region_name': region_02['region_name'],
+            }
+            ],
 
-        'suppliers_in_regions': [
-            supplier_in_region_SA01R01,
-            supplier_in_region_SA01R02,
-            supplier_in_region_SA02R01,
-            supplier_in_region_SA03R02
+            'suppliers_in_regions': [
+                supplier_in_region_SA01R01,
+                supplier_in_region_SA01R02,
+                supplier_in_region_SA02R01,
+                supplier_in_region_SA03R02
+            ]
+        },
         ]
-    },
-    ]
+
+    return [{
+            'suppliers': [{
+                'supplier_id': supplier_active_01['supplier_id'],
+                'supplier_name': supplier_active_01['supplier_name'],
+            },
+                {
+                'supplier_id': supplier_active_02['supplier_id'],
+                'supplier_name': supplier_active_02['supplier_name'],
+            },
+                {
+                'supplier_id': supplier_active_03['supplier_id'],
+                'supplier_name': supplier_active_03['supplier_name'],
+            },
+
+                {
+                'supplier_id': supplier_inactive_01['supplier_id'],
+                'supplier_name': supplier_inactive_01['supplier_name'],
+            }
+            ],
+
+            'regions': [{
+                'region_id': region_01['region_id'],
+                'region_name': region_01['region_name'],
+            },
+                {
+                'region_id': region_02['region_id'],
+                'region_name': region_02['region_name'],
+            }
+            ],
+
+            'suppliers_in_regions': [
+                supplier_in_region_SA01R01,
+                supplier_in_region_SA01R02,
+                supplier_in_region_SA02R01,
+                supplier_in_region_SA03R02,
+                supplier_in_region_SIA01R01
+            ]
+            }]
+
+
+@app.route('/', methods=['POST', 'GET'])
+def find_regionwise_chargers_and_bookings(filter):
+    '''
+        if post:
+        filter of following format:
+        {
+            start_date: '',
+            end_date: '',
+        }
+
+        if get:
+            return all
+    '''
+    if request.method == 'POST':
+        return [{
+            'bookings': [
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'}],
+
+            'chargers': [
+                {'charger_id', 'cluster_id', 'added_on'},
+                {'charger_id', 'cluster_id', 'added_on'},
+                {'charger_id', 'cluster_id', 'added_on'},
+                {'charger_id', 'cluster_id', 'added_on'},
+                {'charger_id', 'cluster_id', 'added_on'},
+                {'charger_id', 'cluster_id', 'added_on'},
+                {'charger_id', 'cluster_id', 'added_on'}],
+
+            'clusters': [
+                {'cluster_id', 'region_id'},
+                {'cluster_id', 'region_id'},
+                {'cluster_id', 'region_id'},
+                {'cluster_id', 'region_id'}],
+
+            'regions': [
+                {'region_id', 'region_name'},
+                {'region_id', 'region_name'},
+            ],
+        }
+        ]
+
+    return [{
+            'bookings': [
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'}],
+
+            'chargers': [
+                {'charger_id', 'cluster_id', 'added_on'},
+                {'charger_id', 'cluster_id', 'added_on'},
+                {'charger_id', 'cluster_id', 'added_on'},
+                {'charger_id', 'cluster_id', 'added_on'},
+                {'charger_id', 'cluster_id', 'added_on'},
+                {'charger_id', 'cluster_id', 'added_on'},
+                {'charger_id', 'cluster_id', 'added_on'}],
+
+            'clusters': [
+                {'cluster_id', 'region_id'},
+                {'cluster_id', 'region_id'},
+                {'cluster_id', 'region_id'},
+                {'cluster_id', 'region_id'}],
+
+            'regions': [
+                {'region_id', 'region_name'},
+                {'region_id', 'region_name'}
+            ],
+            }
+            ]
+
+
+@app.route('/', methods=['POST', 'GET'])
+def find_frequented_data(filter):
+    '''
+        if post:
+        filter of following format:
+        {
+            start_date: '',
+            end_date: '',
+        }
+
+        if get:
+            return all
+    '''
+    if request.method == 'POST':
+        return [{
+            'bookings': [
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'}],
+
+            'chargers': [
+                {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+                {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+                {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+                {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+                {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+                {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+                {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'}],
+
+            'suppliers': [
+                {'supplier_id', 'supplier_name'},
+                {'supplier_id', 'supplier_name'},
+                {'supplier_id', 'supplier_name'},
+                {'supplier_id', 'supplier_name'}],
+
+            'clusters': [
+                {'cluster_id', 'cluster_name', 'region_id'},
+                {'cluster_id', 'cluster_name', 'region_id'},
+                {'cluster_id', 'cluster_name', 'region_id'},
+                {'cluster_id', 'cluster_name', 'region_id'}],
+
+            'regions': [
+                {'region_id', 'region_name'},
+                {'region_id', 'region_name'}]
+        }]
+
+    return [{
+            'bookings': [
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'},
+                {'booking_id', 'charger_id', 'booking_date', 'cost'}],
+
+            'chargers': [
+                {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+                {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+                {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+                {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+                {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+                {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+                {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'}],
+
+            'suppliers': [
+                {'supplier_id', 'supplier_name'},
+                {'supplier_id', 'supplier_name'},
+                {'supplier_id', 'supplier_name'},
+                {'supplier_id', 'supplier_name'}],
+
+            'clusters': [
+                {'cluster_id', 'cluster_name', 'region_id'},
+                {'cluster_id', 'cluster_name', 'region_id'},
+                {'cluster_id', 'cluster_name', 'region_id'},
+                {'cluster_id', 'cluster_name', 'region_id'}],
+
+            'regions': [
+                {'region_id', 'region_name'},
+                {'region_id', 'region_name'}]
+            }]
+
+
+@app.route('/', methods=['POST', 'GET'])
+def find_frequented_data(filter):
+    return [{
+        'chargers': [
+            {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+            {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+            {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+            {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+            {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+            {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'},
+            {'charger_id', 'pin_type', 'AC/DC', 'supplier_id', 'cluster_id'}],
+
+        'charger_status': [
+            {'charger_id', 'current_status', 'fault_id',
+                'fault_occurence_time', 'fault_recovery_time', 'downtime'},
+            {'charger_id', 'current_status', 'fault_id',
+                'fault_occurence_time', 'fault_recovery_time', 'downtime'},
+            {'charger_id', 'current_status', 'fault_id',
+                'fault_occurence_time', 'fault_recovery_time', 'downtime'},
+            {'charger_id', 'current_status', 'fault_id',
+                'fault_occurence_time', 'fault_recovery_time', 'downtime'},
+            {'charger_id', 'current_status', 'fault_id',
+                'fault_occurence_time', 'fault_recovery_time', 'downtime'},
+            {'charger_id', 'current_status', 'fault_id',
+                'fault_occurence_time', 'fault_recovery_time', 'downtime'},
+            {'charger_id', 'current_status', 'fault_id', 'fault_occurence_time', 'fault_recovery_time', 'downtime'}],
+
+        'faults': [
+            {'fault_id', 'fault_type', 'additional_msg'},
+            {'fault_id', 'fault_type', 'additional_msg'},
+            {'fault_id', 'fault_type', 'additional_msg'}],
+
+        'suppliers': [
+            {'supplier_id', 'supplier_name'},
+            {'supplier_id', 'supplier_name'},
+            {'supplier_id', 'supplier_name'},
+            {'supplier_id', 'supplier_name'}
+        ]
+    }]
 
 
 # main driver function
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Information on Using the App:
+
+This is a dummy server, used for defining the routes and json files.
+
+
+## Tu Run the app:
+
+Change path to your project directory at terminal. Then execute the following commands:
+
+```
+    pip install pipenv
+    pipenv shell
+    pipenv install
+    python3 app.py
+```
+
+***
+
+
+
